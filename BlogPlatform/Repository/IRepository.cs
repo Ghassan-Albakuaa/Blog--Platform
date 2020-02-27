91122537a10f729601b51fd9d984e723b0d6c8cf
Jason <JSilvis030592@yahoo.com>
2020-02-26 20:44:12 -0500

    Added Irepository interface.

---
+++ BlogPlatform/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPlatform.Repository
+{
+    interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        IEnumerable Find(int id);
+        T GetById(int id);
+        void Add(T product);
+        void Update(T product);
+        void Delete(int id);
+        void Save();
+    }
+}
